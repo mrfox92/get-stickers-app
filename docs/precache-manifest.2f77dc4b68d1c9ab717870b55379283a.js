a9b2ea282d24d38189a3f38c8e7e00d1b8d7f94f
David Alvarez <davidalvarez54@gmail.com>
2020-09-05 19:46:34 -0400

    docs Agregado

---
+++ docs/precache-manifest.2f77dc4b68d1c9ab717870b55379283a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00c1ed85c3c33a16cb3f1ccc40f242d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d74b8fbc27abc9c0b43",
+    "url": "/static/css/main.0647afff.chunk.css"
+  },
+  {
+    "revision": "e20c8dbc3f186e7b8756",
+    "url": "/static/js/2.3ea5cf12.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3ea5cf12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d74b8fbc27abc9c0b43",
+    "url": "/static/js/main.cf5bc0f2.chunk.js"
+  },
+  {
+    "revision": "f14a69c87a2252187cac",
+    "url": "/static/js/runtime-main.14d8fbc3.js"
+  }
+]);
