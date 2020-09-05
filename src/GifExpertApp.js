e40bea43ea082041352914f1896f77fb86e7e93c
David Alvarez <davidalvarez54@gmail.com>
2020-09-05 19:39:48 -0400

    First Commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,41 @@
+import React, { Fragment, useState } from 'react';
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+
+const GifExpertApp = () => {
+
+
+    // const categories = ['one Punch', 'Samurai X', 'Dragon Ball'];
+    const [ categories, setCategories ] = useState(['Corgi']);
+
+
+    // const handAdd = () => {
+    //     //  forma 1 spread operator
+    //     // setCategories( [...categories, 'Fullmetal Alchemist Brotherhoods'] );
+    //     //  forma 2 callback + spread operator
+    //     setCategories( elements => [ ...elements, 'Fullmetal Alchemist Brotherhoods' ]);
+    // }
+
+
+
+    return (
+        <Fragment>
+            <h2 className="text-white mb-4">GetStickersApp</h2>
+            <AddCategory setCategories={ setCategories } />
+            <hr/>
+
+            { 
+                categories.map( category => {
+
+                    // return <li key={ category }> { category } </li>;
+                    //  utilizamos nuestro componente
+                    return <GifGrid key={ category } category={ category } />
+                })
+            }
+        </Fragment>
+    )
+}
+
+
+export default GifExpertApp;
---
+++ src/helpers/getGifs.js
@@ -0,0 +1,23 @@
+//  retorna una promesa que resuelve la coleccion de imagenes o gifs
+export const getGifs = async(category, limit = 24) => {
+
+    //  realizamos la petición http para traer los gifs
+    //  con encodeURI() de JS escapa los caracteres extraños y reemplaza los espacios por %20, entre otras cosas
+    const url = `https://api.giphy.com/v1/stickers/search?q=${ category }&limit=${ limit }&api_key=w4U0uVyxFyqlZrHTG3P8cJys72GSBsJs`;
+    // const url = `https://api.giphy.com/v1/stickers/search?q=${ encodeURI( category ) }&limit=${ limit }&api_key=w4U0uVyxFyqlZrHTG3P8cJys72GSBsJs`;
+    const resp = await fetch(url);
+    const { data } = await resp.json();
+
+    // console.log(data);
+
+    const stickers = data.map(img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url
+        }
+    });
+
+    // console.log(stickers);
+    return stickers;
+}
